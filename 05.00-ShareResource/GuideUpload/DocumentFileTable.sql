596088fa1e912b3e187ec88eee7a6921dc92e0c0
PHUONG-PC\Admin <phuongnhse63254@fpt.edu.vn>
2023-06-15 21:24:46 +0700

    add guide line and sql

---
+++ 05.00-ShareResource/GuideUpload/DocumentFileTable.sql
@@ -0,0 +1,12 @@
+﻿create table DocumentFiles
+(
+    Id            int identity
+        constraint PK_DocumentFile
+        primary key,
+    HttpLink      nvarchar(max),
+    CreatedBy     nvarchar(max),
+    Approved      bit,
+    CreatedDate   datetime2,
+--     add foreign key chỗ này
+    MeetingId int
+);
